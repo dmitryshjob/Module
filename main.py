695a38e235602596b2c14cb14987d4b335446fca
Dmitry <dmitryshjod@yandex.ru>
2023-01-03 18:00:49 +0300

    dz_mod

---
+++ main.py
@@ -0,0 +1,21 @@
+
+from application.salary import calculate_salary
+from application.db.people import get_employees
+from datetime import date
+
+
+if __name__ == "__main__":
+   
+
+    calculate_salary()
+    get_employees()
+    current_date = date.today()
+    print(current_date)
+
+
+
+
+
+
+
+
